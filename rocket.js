cb326b1989709670419740ff894d80a82dc75adc
kingbule <bulecreeper.holmes@gmail.com>
2023-07-24 13:21:37 -0400

    Update rocket.js

--- rocket.js
+++ rocket.js
@@ -3,7 +3,7 @@ var countdownNumber = 10;
 
 var changeState = function (state) {
 	document.body.className = 'body-state'+ state;
-    clearInterval(timer)
+    
 
 	if (state == 2) {
         timer = setInterval(function () {
